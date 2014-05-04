a130d295e8a90e53c9d73645131591967d03f2ab
hiromu <home@suzuki-akira-no-iMac.local>
2014-05-04 13:55:43 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KazukiPort::Application.config.secret_key_base = 'f797d247647412029b4636344be10689488016a630ed39874bf7a2006bc860464063c9b8ac730077e602464b87e3ca41412a5f56cc35d87671c013793a556836'
