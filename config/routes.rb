78b6ff917dcadced0d0f813a7657c41929c4782f
hiromu <home@suzuki-akira-no-iMac.local>
2014-05-18 21:06:49 +0900

    アップロード追加

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,13 @@ KazukiPort::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  resources :sculps
+  
+  resources :images do
+    member do
+      get 'show_image'
+    end
+  end
 
   # Example resource route with options:
   #   resources :products do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# coding: utf-8
+
+Author.create(:user_id => 'kazuaki', :name => '鞍谷 一樹')
+Exhib.create(:title => 'くらたの展示',:start_date => '2014-05-10',:end_date => '2014-05-21',:gallery => 'かすきん家',:gallery_url => 'http://oriori.index',:adress => '相原',:desc => 'てきとーにやってます')
+Sculp.create(:title => 'one',:pic_id => '01',:wide => '100',:depth => '100',:high => '100',:madein =>'2010-04-01')
+Sculp.create(:title => 'two',:pic_id => '02',:wide => '20',:depth => '20',:high => '20',:madein =>'2009-04-01')
+Sculp.create(:title => 'three',:pic_id => '03',:wide => '1',:depth => '5',:high => '6',:madein =>'2000-04-01')
